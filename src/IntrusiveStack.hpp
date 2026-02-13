7da275b288cc0507ea692cf9fa0825a0175cc9b6
TheAlmightyWale <thealmightywale@gmail.com>
2026-02-13 20:08:52 +1300

    first commit

---
+++ src/IntrusiveStack.hpp
@@ -0,0 +1,98 @@
+#pragma once
+#include <cstdint>
+#include <cstdlib>
+#include <assert.h>
+#include <functional>
+
+namespace Jalloc
+{
+    namespace Detail
+    {
+        template <typename T>
+        struct BookKeeping
+        {
+            uint16_t sizeBytes = sizeof(T);
+            std::function<void(void const *)> destructor = ([](void const *p)
+                                                            { static_cast<T const *>(p)->~T(); });
+        };
+
+        template <>
+        struct BookKeeping<void>
+        {
+            uint16_t sizeBytes;
+            std::function<void(void const *)> destructor;
+        };
+    }
+    // Fixed size Intrusive stack class. Can allocate and deallocate memory
+    //   all bookkeeping is held between allocations
+    //   can optionally provide usage statistics
+    class IntrusiveStack
+    {
+    public:
+        IntrusiveStack(uint32_t sizeBytes)
+            : maxSize(sizeBytes)
+        {
+            base = static_cast<uint8_t *>(std::malloc(maxSize));
+            head = base;
+        }
+
+        ~IntrusiveStack()
+        {
+            std::free(base);
+        }
+
+        uint32_t AvailableSpace()
+        {
+            return static_cast<uint32_t>(maxSize - (head - base));
+        }
+
+        template <typename T, typename... Args>
+        [[nodiscard]] T *Alloc(Args &&...args) noexcept
+        {
+            static_assert(sizeof(T) < std::numeric_limits<uint16_t>::max(), "Can't allocate an individual object larger then 65,535 bytes");
+            // optional logging
+
+            // Do size check
+            assert((head + sizeof(T) + sizeof(Detail::BookKeeping<T>)) - base < maxSize);
+            T *obj = new (head) T(std::forward<Args>(args)...);
+            head += sizeof(T);
+
+            // create bookkeeping
+            new(head) Detail::BookKeeping<T>();
+            // Attempt to construct object
+            head += sizeof(Detail::BookKeeping<T>);
+
+            // return pointer
+            return obj;
+        }
+
+        void Reset() noexcept
+        {
+            while (head != base)
+            {
+                assert(head > base);
+                Delete();
+            }
+        }
+
+        // Deletes the object at the top of the stack
+        // Whatever it may be
+        void Delete() noexcept
+        {
+            // grab bookkeeper
+            uint8_t *bkStart = head - sizeof(Detail::BookKeeping<void>);
+            Detail::BookKeeping<void> *bk = static_cast<Detail::BookKeeping<void> *>((void *)bkStart);
+            uint8_t *objStart = bkStart - bk->sizeBytes;
+            // call destructor
+            bk->destructor(objStart);
+
+            // decrement head
+            head = objStart;
+        }
+
+    private:
+        uint32_t maxSize = 0;
+        uint8_t *base = nullptr;
+        uint8_t *head = nullptr;
+    };
+}
---
+++ test/IntrusiveStackTests.cpp
@@ -0,0 +1,72 @@
+#include <gtest/gtest.h>
+#include "IntrusiveStack.hpp"
+
+using namespace Jalloc;
+
+#define JALLOC_UNUSED(x) (void)(x)
+
+TEST(IntrusiveStack, CallConstructorOnAllocation)
+{
+    struct DoThinger
+    {
+        DoThinger(int arg)
+        :arg(arg)
+        {
+            constructorWasRun = true;
+        }
+
+        bool constructorWasRun = false;
+        int arg = 0;
+    };
+
+    IntrusiveStack stack(32);
+    DoThinger *dt = stack.Alloc<DoThinger>(1);
+    EXPECT_TRUE(dt->constructorWasRun);
+    EXPECT_EQ(dt->arg, 1);
+}
+
+TEST(IntrusiveStack, CallDestructorOnAllocation)
+{
+    static bool baseDestroyed = false;
+    static bool derivedDestroyed = false;
+
+    struct Base{
+        ~Base(){
+            baseDestroyed = true;
+        }
+    };
+
+    struct Derived: public Base{
+        ~Derived(){
+            derivedDestroyed = true;
+        }
+    };
+
+    IntrusiveStack stack(32);
+    Base* b = stack.Alloc<Derived>();
+    JALLOC_UNUSED(b);
+
+    stack.Delete();
+    EXPECT_TRUE(baseDestroyed);
+    EXPECT_TRUE(derivedDestroyed);
+}
+
+TEST(IntrusiveStack, DeletesTop)
+{
+    IntrusiveStack stack(32);
+    JALLOC_UNUSED(stack.Alloc<int>());
+    JALLOC_UNUSED(stack.Alloc<int64_t>());
+
+    stack.Delete();
+    EXPECT_EQ(stack.AvailableSpace(),28);
+}
+
+TEST(IntrusiveStack, ResetsAll)
+{
+    IntrusiveStack stack(32);
+    JALLOC_UNUSED(stack.Alloc<int>());
+    JALLOC_UNUSED(stack.Alloc<int64_t>());
+
+    stack.Reset();
+    EXPECT_EQ(stack.AvailableSpace(),32);
+}
